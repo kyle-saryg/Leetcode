00b801bfbe74c50751c217c93b4451e5e8e6a950
Kyle Sarygin <kyle.cuyos13@gmail.com>
2023-10-26 14:05:02 +0800

    Update contains_duplicates_test.go

--- 217_Contains_Duplicates/contains_duplicates_test.go
+++ 217_Contains_Duplicates/contains_duplicates_test.go
@@ -1,13 +1,14 @@
 package contains_duplicates
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestContainsDuplicates(t *testing.T) {
 	testCases := []struct {
 		id       int
-		input    []int
+		nums     []int
 		expected bool
 	}{
 		{0, []int{1, 2, 3, 1}, true},
@@ -16,10 +17,10 @@ func TestContainsDuplicates(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		t.Run("", func(t *testing.T) {
-			output := containsDuplicates(tc.input)
+		t.Run(fmt.Sprintf("Test Case #%v", tc.id), func(t *testing.T) {
+			output := containsDuplicates(tc.nums)
 			if tc.expected != output {
-				t.Errorf("case %v\n input: %v\n expected: %v\n returned: %v", tc.id, tc.input, tc.expected, output)
+				t.Errorf("\n input: %v\n expected: %v\n returned: %v", tc.nums, tc.expected, output)
 			}
 		})
 	}
