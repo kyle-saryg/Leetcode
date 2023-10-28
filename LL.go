31c66850f5fb2eb6e35e7b34ac1a2248d5bae537
Kyle Sarygin <kyle.cuyos13@gmail.com>
2023-10-28 19:14:37 +0800

    Update LL.go

--- LL.go
+++ LL.go
@@ -1,5 +1,9 @@
 package linkedList
 
+import (
+	"fmt"
+)
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -15,6 +19,36 @@ func initList(val int) *ListNode {
 }
 
 // Creates a new node, appends to end of list
-func (*ListNode) append(val int) {
+func (head *ListNode) append(val int) {
+	// Traversing Linked List (stops at last node)
+	var nodePtr *ListNode = head
+	for nodePtr.Next != nil {
+		nodePtr = nodePtr.Next
+	}
 
+	// New node creation and insertion
+	newNode := createNode(val)
+	nodePtr.Next = newNode
+}
+
+// Slice is appended in-order
+func (head *ListNode) appendSlice(val []int) {
+
+	return nil
+}
+
+func createNode(val int) *ListNode {
+	newNode := new(ListNode)
+	newNode.Val = val
+	newNode.Next = nil
+
+	return newNode
+}
+
+func (head *ListNode) display() {
+	for head != nil {
+		fmt.Printf("<%v> -> ", head.Val)
+		head = head.Next
+	}
+	fmt.Printf("\n")
 }
