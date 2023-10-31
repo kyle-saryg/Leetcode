9cea97a71549eb0c93d7b3791bbd8594ebf5b26f
Kyle Sarygin <kyle.cuyos13@gmail.com>
2023-10-31 12:42:14 +0800

    Fixed unit tests

--- 206_reverse_linked_list_2/reverse_linked_list2_test.go
+++ 206_reverse_linked_list_2/reverse_linked_list2_test.go
@@ -21,18 +21,18 @@ func TestReverseList(t *testing.T) {
 
 	testCases := []struct {
 		id       int
-		input    *linkedList.ListNode
-		expected *linkedList.ListNode
+		input    []int
+		expected []int
 	}{
-		{0, linkedList.CreateList([]int{1, 2, 3, 4, 5}), linkedList.CreateList([]int{5, 4, 3, 2, 1})},
-		{1, linkedList.CreateList([]int{1, 2}), linkedList.CreateList([]int{2, 1})},
-		{2, linkedList.CreateList([]int{}), linkedList.CreateList([]int{})},
+		{0, []int{1, 2, 3, 4, 5}, []int{5, 4, 3, 2, 1}},
+		{1, []int{1, 2}, []int{2, 1}},
+		{2, []int{}, []int{}},
 	}
 	for _, tc := range testCases {
 		t.Run(fmt.Sprintf("Test Case #%v", tc.id), func(t *testing.T) {
-			output := reverseList(tc.input)
-			if !linkedList.Compare(output, tc.expected) {
-				t.Errorf("\n case: %v\n output: %v\n expected: %v", linkedList.ToSlice(tc.input), linkedList.ToSlice(output), linkedList.ToSlice(tc.expected))
+			output := reverseList(linkedList.CreateList(tc.input))
+			if !linkedList.Compare(output, linkedList.CreateList(tc.expected)) {
+				t.Errorf("\n case: %v\n output: %v\n expected: %v", tc.input, linkedList.ToSlice(output), tc.expected)
 			}
 		})
 	}
