61fdeadb1ae1a29f8b9a04d93b86cbfa10e461aa
Kyle Sarygin <kyle.cuyos13@gmail.com>
2023-10-31 10:57:32 +0800

    Create reverse_linked_list2.go

---
+++ 206_reverse_linked_list/reverse_linked_list2.go
@@ -0,0 +1,33 @@
+package reverseLinkedList
+
+import (
+	. "github.com/kyle-saryg/Leetcode/linkedList"
+)
+
+/*
+Algorithm: (Sacrificing time complexity for more efficient memory complexity)
+ - Step A:
+   -- Move ptr1 to first node
+   -- Move ptr2 to last node
+   -- Swap values
+
+ - Step B:
+   -- ptr1 moves forward one node
+   -- ptr2 moves backard one node
+   -- swap
+
+ - Repeat Step A and B until:
+   -- ptr1 and ptr2 point at the same node
+      OR
+   -- ptr1 is one node behind ptr2
+
+Space Complexity: O(1)
+ - In-place algorithm
+ - Unlike first algorithm
+   -- slice which grows with the size of the LL
+ - No additional memory is allocated
+*/
+
+func reverseList(head *ListNode) *ListNode {
+	return nil
+}
