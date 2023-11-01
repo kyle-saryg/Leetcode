5fce42afe8d6bf1643571779eefaa45f6ce33b7e
Kyle Sarygin <kyle.cuyos13@gmail.com>
2023-11-01 13:02:53 +0800

    Update merge_two_sorted_lists.go

--- 21_merge_two_sorted_lists/merge_two_sorted_lists.go
+++ 21_merge_two_sorted_lists/merge_two_sorted_lists.go
@@ -4,6 +4,21 @@ import (
 	. "github.com/kyle-saryg/Leetcode/linkedList"
 )
 
+/*
+algorithm:
+ -- curr1 starts at head of list1
+ -- next1 is one node ahead of curr1
+ -- curr2 starts at head of list2
+ -- next2 is one node ahead of curr2
+
+ -- Compare values of curr1 and curr2
+ -- Example: curr1 is less than curr2
+   -- curr1.next = curr2
+   -- curr2.next = next1
+   -- curr1 = next1
+   -- next1 = next1.next
+*/
+
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 
 	return nil
