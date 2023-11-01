1281277be6a4583d4cfc64d928f334108359dca5
Kyle Sarygin <kyle.cuyos13@gmail.com>
2023-11-01 12:16:55 +0800

    Create merge_two_sorted_lists_test.go

---
+++ 21_merge_two_sorted_lists/merge_two_sorted_lists_test.go
@@ -0,0 +1,31 @@
+package mergeTwoSortedLists
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/kyle-saryg/Leetcode/linkedList"
+)
+
+func TestMergeTwoLists(t *testing.T) {
+	testCases := []struct {
+		id       int
+		list1    []int
+		list2    []int
+		expected []int
+	}{
+		{0, []int{1, 2, 4}, []int{1, 3, 4}, []int{1, 1, 2, 3, 4}},
+		{1, []int{}, []int{}, []int{}},
+		{2, []int{}, []int{0}, []int{0}},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("Test Case #%v", tc.id), func(t *testing.T) {
+			output := mergeTwoLists(linkedList.CreateList(tc.list1), linkedList.CreateList(tc.list2))
+
+			if !linkedList.Compare(output, linkedList.CreateList(tc.expected)) {
+
+			}
+
+		})
+	}
+}
