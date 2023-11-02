e496922e76b267c2b3601a2895a5b4a1e501aa30
Kyle Sarygin <kyle.cuyos13@gmail.com>
2023-11-03 00:11:10 +0800

    Create linked_list_cycle_test.go

---
+++ 141_linked_list_cycle_2/linked_list_cycle_test.go
@@ -0,0 +1,52 @@
+package linkedListCycle2
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/kyle-saryg/Leetcode/linkedList"
+)
+
+func TestHasCycle(t *testing.T) {
+	testCases := []struct {
+		id       int
+		input    []int
+		expected bool
+	}{
+		{0, []int{3, 2, 0, -4}, true},
+		{0, []int{1, 2}, true},
+		{0, []int{1}, false},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("Test Case #%v\n", tc.id), func(t *testing.T) {
+			// Create linked list
+			// Add the loop based on the 'expected' boolean
+			ll := linkedList.CreateList(tc.input)
+			if tc.expected {
+				createLoop(ll)
+			}
+
+			result := hasCycle(ll)
+			if result != tc.expected {
+				t.Errorf("\t\n--input: %v \n\t--expected: %v \n\t--result: %v\n", tc.input, tc.expected, result)
+			}
+
+		})
+	}
+}
+
+func createLoop(head *linkedList.ListNode) {
+	// Traverse to the last node of the list
+	// Set next value to head
+	//   -- Technically should set to a random node in the list
+	//   -- However, setting it back to the head should suffice
+
+	explorer := head
+
+	for explorer.Next != nil {
+		explorer = explorer.Next
+	}
+
+	explorer.Next = head
+}
