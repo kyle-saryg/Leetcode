4fd9b28b4e55e15341615d93437f942a20351179
Kyle Sarygin <kyle.cuyos13@gmail.com>
2023-11-02 14:54:17 +0800

    Update merge_two_sorted_lists.go

--- 21_merge_two_sorted_lists/merge_two_sorted_lists.go
+++ 21_merge_two_sorted_lists/merge_two_sorted_lists.go
@@ -10,8 +10,13 @@ c2 starts at list2 head
 
 note: if c2 is nil, inserted all of list2 into list1, prevents seg fault
 while c1 != nil and c2 != nil
-  -- if c1 is GE c2
-    -- insert c2 behind c1
+  -- n1 = c1.Next
+  -- n2 = n2.Next
+
+  -- if c1 is LE c2
+    -- insert c2 ahead c1
+	-- c1 = c2 // Inserted c2 into list 1
+	-- c2 = next2 // Moving c2 back into list 2
 
 if c2 != nil
   -- Append the rest of list2 to list1
@@ -21,12 +26,15 @@ helper funcs:
 
 */
 /*
-	Example:
+Example:
+	c1 = list 1 current
+	n2 = leading c1 // Used to connect list 1 when inserting c2
+	c2 = list 2 current
+	n2 = leading c2 // Used to move c2 back to list 2 (Post - Insertion)
+
+
 	list 1: [1][2][4][nil]
-			c1
 	list 2: [1][3][4][nil]
-			c2  n2
-			p1
 	--
 */
 
